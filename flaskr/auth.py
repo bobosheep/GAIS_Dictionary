e2cbd9e5bf17c8dfba9bc7fe59ba3acac3dfc3ef
Ian Tsai <ianpaul32@gmail.com>
2020-10-21 18:50:42 +0800

    Add user logging

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,6 +1,7 @@
 import functools
 import uuid
 import hashlib
+import json
 from datetime import datetime
 
 from flask import (
@@ -8,12 +9,102 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskr.db import User
+from flaskr.db import User, CategoryNode
 
 tokens = dict()
 user_token = dict()
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+def user_logging(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        response = view(**kwargs)
+
+        method = {
+            'GET': '瀏覽',
+            'POST' : '新增',
+            'PUT' : '更新',
+            'DELETE': '刪除'
+        }
+        
+        LEVEL = {
+            'GET': 0, 
+            'POST' : 1,
+            'PUT' : 1,
+            'DELETE': 1
+        }
+
+        user = g.user
+        username = 'Guest' if user is None else user.uname
+        user_ip = request.host
+        action_time = datetime.now().strftime("%Y-%m-%d %H:%M")
+        action_part = request.path
+        action = method[request.method]
+        level = LEVEL[request.method] if response[1] / 100  < 5 else 2
+        print_level = 'INFO' if level == 0 else 'WARN' if level == 1 else 'ERROR'
+        request_failed = '成功' if response[1] / 100 < 3 else '失敗'
+        
+        if request.blueprint == 'auth':
+            if request.endpoint == 'auth.login':
+                action_part = ''
+                action = '登入'
+            elif request.endpoint == 'auth.logout':
+                action_part = ''
+                action = '登出'
+
+            
+        elif request.blueprint == 'cat':
+            if request.endpoint == 'cat.category_api':
+                args = action_part.split('/')
+                if len(args) > 2:
+                    part = args[2] 
+                    cat = CategoryNode.objects(cid=part).first()
+                    if part == '':
+                        action_part = '全部類別'
+                    elif cat is not None:
+                        action_part = cat.cname +' 類別'
+                    else:
+                        action_part = '未定義類別'
+                    if request.method == 'POST':
+                        action_part = request.form['cname']
+                    
+            elif request.endpoint == 'cat.category_seeds_api':
+                args = action_part.split('/')
+                if len(args) > 2:
+                    part = args[2] 
+                    cat = CategoryNode.objects(cid=part).first()
+                    if part == 'seeds':
+                        action_part = '全部類別的種子'
+                    elif cat is not None:
+                        action_part = cat.cname +' 類別的種子'
+                    else:
+                        action_part = '未定義類別'
+                    if request.method == 'POST' or request.method == 'DELETE':
+                        action_part = cat.cname +'的種子 ' + request.form['seeds']
+                    
+            elif request.endpoint == 'cat.category_terms_api':
+                args = action_part.split('/')
+                if len(args) > 2:
+                    part = args[2] 
+                    cat = CategoryNode.objects(cid=part).first()
+                    if part == 'seeds':
+                        action_part = '全部類別的種子'
+                    elif cat is not None:
+                        action_part = cat.cname +' 類別的種子'
+                    else:
+                        action_part = '未定義類別'
+                    if request.method == 'POST' or request.method == 'DELETE':
+                        action_part = cat.cname +'的詞 ' + request.form['terms']
+            elif request.endpoint == 'cat.getCategoryStat':
+                action_part = '目前類別狀態'
+        else:
+            pass
+
+        print(f'[{print_level}] User {username} {action} {action_part} {request_failed} @ {action_time} from {user_ip}')
+        
+        return response
+    return wrapped_view
 
 @bp.route('/register', methods=['POST'])
 def register():
@@ -55,6 +146,7 @@ def register():
 
 
 @bp.route('/login', methods=['POST'])
+@user_logging
 def login():
     '''     For user login      '''
 
@@ -76,6 +168,7 @@ def login():
         if error is None:
             session['user_id'] = user.uid
             user.last_login = datetime.now()
+            g.user = user
             # md5 = hashlib.md5()
             # user_str = user.uname + datetime.now().strftime('%m/%d/%Y %H:%M:%S')
             # md5.update(s.encode('utf-8'))
@@ -125,14 +218,6 @@ def login_required(view):
     return wrapped_view
 
 
-def user_logging(view):
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        print(view)
-        print(**kwargs)
-        return view(**kwargs)
-
-    return wrapped_view
 
 def admin_only(view):
     @functools.wraps(view)
@@ -148,6 +233,7 @@ def admin_only(view):
 
     
 @bp.route('/logout', methods=['POST'])
+@user_logging
 @login_required
 def logout():
     '''     For user log out    '''
