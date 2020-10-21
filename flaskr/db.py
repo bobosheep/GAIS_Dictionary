0eca4016c190709fb5a20f58be081464a85b6c82
Ian Tsai <ianpaul32@gmail.com>
2020-10-21 18:52:14 +0800

    Modify some collection field

--- flaskr/db.py
+++ flaskr/db.py
@@ -5,7 +5,7 @@ connect('test', host='localhost', port=27017)
 class User(DynamicDocument):
     uid = UUIDField(binary=False)
     social_uid = StringField()
-    uname = StringField()
+    uname = StringField(unique=True)
     display_name = StringField(max_length=12, min_length=2)
     password = StringField()
     level = IntField(min_value=0, max_value=5)
@@ -17,11 +17,12 @@ class User(DynamicDocument):
     prefer = DictField()
 
 class UserLog(Document):
-    level = IntField()
-    action = StringField()
-    action_date = DateField()
+    level = IntField()          # 0: INFO, 1: WARN, 2:ERROR, 3: FATAL 
+    action = IntField()         # 0: GET, 1: POST, 2: PUT, 3: DELETE
     action_time = DateTimeField()
-    action_part = StringField()
+    action_part = GenericReferenceField()
+    action_description = StringField()
+    action_stat = BooleanField()
     user = ReferenceField(User)
     user_ip = StringField()
 
@@ -29,14 +30,14 @@ class UserLog(Document):
 
 class Term(Document):
     tid = UUIDField(binary=False)
-    tname = StringField()
+    tname = StringField(unique=True)
 
     meta = {'allow_inheritance': True}
 
 
 class Category(Document):
     cid = UUIDField(binary=False)
-    cname = StringField()
+    cname = StringField(unique=True)
     created = DateTimeField()
     last_updated = DateTimeField()
     is_root = BooleanField()
@@ -47,11 +48,12 @@ class Category(Document):
     view_cnt = IntField()
     edit_cnt = IntField()
     published = BooleanField()
-
+    model = StringField()
     meta = {'allow_inheritance': True}
 
 class CategoryNode(Category):
     parent = ReferenceField(Category)
+    has_children = BooleanField()
     children = ListField(ReferenceField(Category))
     meta = {'allow_inheritance': True}
 
@@ -66,7 +68,7 @@ class CategoryLeaf(CategoryNode):
 class TermDetail(Term):
     categories = ListField(ReferenceField(Category))
     tags = ListField(StringField())
-    alias = ListField(StringField())
+    aliases = ListField(StringField())
     synonym = ListField(StringField())
     related_synonym = ListField(StringField())
     antonym = ListField(StringField())
